afb0c8a0b1557666b5e3c8e2d5bff6e75c353f12
Abdul M <120386636+ctrlwinrepair@users.noreply.github.com>
2023-02-05 13:14:50 -0700

    Add files via upload

---
+++ librewolfinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+distro=$(if echo " bullseye focal impish jammy uma una " | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
+echo "deb [arch=amd64] http://deb.librewolf.net $distro main" | sudo tee /etc/apt/sources.list.d/librewolf.list
+sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg
+sudo apt update
+sudo apt install librewolf -y
