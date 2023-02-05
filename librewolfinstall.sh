ba3b1af7a8c55c13e6640c4e995b96c4783e8168
Abdul M <120386636+ctrlwinrepair@users.noreply.github.com>
2023-02-05 13:22:48 -0700

    Update librewolfinstall.sh

--- librewolfinstall.sh
+++ librewolfinstall.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 distro=$(if echo " bullseye focal impish jammy uma una " | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
+#Distro check
 echo "deb [arch=amd64] http://deb.librewolf.net $distro main" | sudo tee /etc/apt/sources.list.d/librewolf.list
+#Imports the apt source
 sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg
+#Imports the signed key, attach to keyring
 sudo apt update
+#Update
 sudo apt install librewolf -y
+#Install librewolf from apt
